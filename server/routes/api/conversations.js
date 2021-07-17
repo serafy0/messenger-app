17a8ff3c1291add96fa783690e815f00cc38e991
mohamedelserafy <elserafy02@gmail.com>
2021-07-17 14:43:07 +0200

    bug fix
    this should fix the messages order

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -22,7 +22,7 @@ router.get("/", async (req, res, next) => {
       attributes: ["id"],
       order: [[Message, "createdAt", "DESC"]],
       include: [
-        { model: Message, order: ["createdAt", "DESC"] },
+        { model: Message, order: ["createdAt", "ASC"] },
         {
           model: User,
           as: "user1",
@@ -69,6 +69,8 @@ router.get("/", async (req, res, next) => {
 
       // set properties for notification count and latest message preview
       convoJSON.latestMessageText = convoJSON.messages[0].text;
+      convoJSON.messages.reverse()
+
       conversations[i] = convoJSON;
     }
     res.json(conversations);
