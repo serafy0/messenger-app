7629945cf170316e5055ffbeb04b37bef0e378a4
mohamedelserafy <elserafy02@gmail.com>
2021-07-22 22:41:59 +0200

    added tests

--- server/test/ping.spec.js
+++ server/test/ping.spec.js
@@ -1,21 +1,79 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const app = require("../app.js");
-
+const { app } = require("../app.js");
+const User = require("../db/models/user");
+const { db } = require("../db/db");
 chai.should();
 chai.use(chaiHttp);
 
-describe("/POST ping", () => {
-  it("it should return 400", (done) => {
+describe("POST /login", () => {
+  it("it should return 200 when logging in with a valid credentials ", (done) => {
+    process.env.SESSION_SECRET = "secret";
     chai
       .request(app)
-      .post(`/ping/`)
-      .send({ teamName: "Shums" })
+      .post("/auth/login")
+
+      .send({
+        username: "thomas",
+        password: "123456"
+      })
+      .end((err, res) => {
+        res.status.should.equal(200);
+        res.body.token.should.exist;
+        done();
+      });
+  });
+  it("it should return an error when logging in with a bad credentials ", (done) => {
+    chai
+      .request(app)
+      .post("/auth/login")
+      .send({ username: "wrong", password: "password" })
+      .end((err, res) => {
+        res.should.have.status(401);
+
+        done();
+      });
+  });
+});
+
+describe(" POST /register", () => {
+  before(async () => {
+    //This is sequelize syntax for postgres. You could just take your mongoose syntax here to delete all users
+    await User.destroy({
+      where: { username: "test" },
+      truncate: { cascade: true }
+    });
+  });
+
+  it("it should register a new user and return 200", (done) => {
+    chai
+      .request(app)
+      .post("/auth/register")
+
+      .send({
+        password: "test123",
+        username: "test",
+        email: "test@test.com"
+      })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.token.should.exist;
+        done();
+      });
+  });
+  it("it should return an error if data is invalid", (done) => {
+    chai
+      .request(app)
+      .post("/auth/register")
+
+      .send({
+        password: "short",
+        username: "test",
+        email: "invalid-mail"
+      })
       .end((err, res) => {
         res.should.have.status(400);
-        res.body.should.have
-          .property("response")
-          .eql("Shums is not part of the team. Modify your .env");
+        res.body.error.should.exist;
         done();
       });
   });
